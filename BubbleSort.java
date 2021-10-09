4fb9857dabc4e5a3d843c0ca481b2cc1055d0e56
Abhishek Sharma <58878438+imgeekabhi@users.noreply.github.com>
2021-10-09 22:25:09 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,21 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import sort.*;
-
-class BubbleSort{
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter the length of the array");
-        int n=Integer.parseInt(br.readLine());
-        int arr[] = Sort.getInputArray(n);
-        bubbleSort(arr, n);
-        Sort.printArray(arr);
+import java.util.Scanner;
+public class BubbleSort {
+    public static void bubbleSort(int[] arr){
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            boolean flag=false;
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    flag = true;
+                }
+            }
+            if(flag)
+                break;            
+        }
     }
-
-    private static void bubbleSort(int arr[], int n){
-        // complete this function.
-    }
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0 ; i<arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Before Swapping : ");
+        for(int elements : arr){
+            System.out.print(elements+", ");
+        }
+        //Method Calling
+        bubbleSort(arr);
+        System.out.println("After Swapping : ");
+        for(int elements : arr){
+            System.out.print(elements+", ");
+        }
+    }    
 }
